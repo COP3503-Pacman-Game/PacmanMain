9a19b75942ac9eb9bf98fe520eac010b63bf2be0
Kassandra Crompton <kcrompton@ufl.edu>
2016-12-01 18:15:57 -0500

    Pacman Main

---
+++ Pacman.cpp
@@ -0,0 +1,192 @@
+#include "Pacman.h"
+using namespace std;
+
+bool gameOver;
+bool validInput;
+char input;
+string direction;
+Player* pacman = new (struct Player);
+
+int GameBoard [22][19]= {
+{ WALL  ,WALL  ,WALL  ,WALL  ,WALL  ,WALL  ,WALL  ,WALL  ,WALL  ,WALL  ,WALL  ,WALL  ,WALL  ,WALL  ,WALL  ,WALL  ,WALL  ,WALL  ,WALL  },
+{ WALL  ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,WALL  ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,WALL  },
+{ WALL  ,EMPTY ,WALL  ,WALL  ,EMPTY ,WALL  ,WALL  ,WALL  ,EMPTY ,WALL  ,EMPTY ,WALL  ,WALL  ,WALL  ,EMPTY ,WALL  ,WALL  ,EMPTY ,WALL  },
+{ WALL  ,EMPTY ,WALL  ,WALL  ,EMPTY ,WALL  ,WALL  ,WALL  ,EMPTY ,WALL  ,EMPTY ,WALL  ,WALL  ,WALL  ,EMPTY ,WALL  ,WALL  ,EMPTY ,WALL  },
+{ WALL  ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,WALL  },
+{ WALL  ,EMPTY ,WALL  ,WALL  ,EMPTY ,WALL  ,EMPTY ,WALL  ,WALL  ,WALL  ,WALL  ,WALL  ,EMPTY ,WALL  ,EMPTY ,WALL  ,WALL  ,EMPTY ,WALL  },
+{ WALL  ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,WALL  ,EMPTY ,EMPTY ,EMPTY ,WALL  ,EMPTY ,EMPTY ,EMPTY ,WALL  ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,WALL  },
+{ WALL  ,WALL  ,WALL  ,WALL  ,EMPTY ,WALL  ,WALL  ,WALL  ,EMPTY ,WALL  ,EMPTY ,WALL  ,WALL  ,WALL  ,EMPTY ,WALL  ,WALL  ,WALL  ,WALL  },
+{ EMPTY ,EMPTY ,EMPTY ,WALL  ,EMPTY ,WALL  ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,WALL  ,EMPTY ,WALL  ,EMPTY ,EMPTY ,EMPTY },
+{ WALL  ,WALL  ,WALL  ,WALL  ,EMPTY ,WALL  ,EMPTY ,WALL  ,WALL  ,PINKY ,WALL  ,WALL  ,EMPTY ,WALL  ,EMPTY ,WALL  ,WALL  ,WALL  ,WALL  },
+{ WRAPL ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,WALL  ,INKY  ,BLINKY,CLYDE ,WALL  ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,WRAPR },
+{ WALL  ,WALL  ,WALL  ,WALL  ,EMPTY ,WALL  ,EMPTY ,WALL  ,WALL  ,WALL  ,WALL  ,WALL  ,EMPTY ,WALL  ,EMPTY ,WALL  ,WALL  ,WALL  ,WALL  },
+{ EMPTY ,EMPTY ,EMPTY ,WALL  ,EMPTY ,WALL  ,EMPTY ,EMPTY ,EMPTY, PACMAN,EMPTY ,EMPTY ,EMPTY ,WALL  ,EMPTY ,WALL  ,EMPTY ,EMPTY ,EMPTY },
+{ WALL  ,WALL  ,WALL  ,WALL  ,EMPTY ,WALL  ,EMPTY ,WALL  ,WALL  ,WALL  ,WALL  ,WALL  ,EMPTY ,WALL  ,EMPTY ,WALL  ,WALL  ,WALL  ,WALL  },
+{ WALL  ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,WALL  ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,WALL  },
+{ WALL  ,EMPTY ,WALL  ,WALL  ,EMPTY ,WALL  ,WALL  ,WALL  ,EMPTY ,WALL  ,EMPTY ,WALL  ,WALL  ,WALL  ,EMPTY ,WALL  ,WALL  ,EMPTY ,WALL  },
+{ WALL  ,EMPTY ,EMPTY ,WALL  ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,WALL  ,EMPTY ,EMPTY ,WALL  },
+{ WALL  ,WALL  ,EMPTY ,WALL  ,EMPTY ,WALL  ,EMPTY ,WALL  ,WALL  ,WALL  ,WALL  ,WALL  ,EMPTY ,WALL  ,EMPTY ,WALL  ,EMPTY ,WALL  ,WALL  },
+{ WALL  ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,WALL  ,EMPTY ,EMPTY ,EMPTY ,WALL  ,EMPTY ,EMPTY ,EMPTY ,WALL  ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,WALL  },
+{ WALL  ,EMPTY ,WALL  ,WALL  ,WALL  ,WALL  ,WALL  ,WALL  ,EMPTY ,WALL  ,EMPTY ,WALL  ,WALL  ,WALL  ,WALL  ,WALL  ,WALL  ,EMPTY ,WALL  },
+{ WALL  ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,EMPTY ,WALL  },
+{ WALL  ,WALL  ,WALL  ,WALL  ,WALL  ,WALL  ,WALL  ,WALL  ,WALL  ,WALL , WALL  ,WALL  ,WALL  ,WALL  ,WALL  ,WALL  ,WALL  ,WALL  ,WALL  }
+};
+
+void Setup() {
+	gameOver = false;
+}
+
+void Draw() {
+	for (int i = 0; i < 22; i++) {
+		for (int j = 0; j < 19; j++) {
+			if (GameBoard[i][j] == WALL) {
+				cout << "#";
+			}
+			else if (GameBoard[i][j] == EMPTY || GameBoard[i][j] == WRAPL || GameBoard[i][j] == WRAPR) {
+				cout << " ";
+			}
+			else if (GameBoard[i][j] == PINKY || GameBoard[i][j] == INKY || GameBoard[i][j] == BLINKY || GameBoard[i][j] == CLYDE) {
+				cout << "G";
+			}
+			else if (GameBoard[i][j] == PACMAN) {
+				cout << "X";
+			}
+		}
+		cout << endl;
+	}
+}
+
+void Input() {
+	validInput = false;
+	direction = " ";
+
+	while (validInput == false) {
+		cin >> input;
+		if (cin.fail()) {
+			cin.clear();
+			cin.ignore(256, '\n');
+			bool validInput = false;
+		}
+		else {
+			switch (input) {
+			case 'n':
+				validInput = true;
+				direction = "NEW GAME";
+				break;
+			case 'p':
+				direction = "PAUSE";
+				validInput = true;
+				break;
+			case 'q':
+				direction = "QUIT";
+				validInput = true;
+				break;
+			case 'w':
+				direction = "UP";
+				validInput = true;
+				break;
+			case 'a':
+				direction = "LEFT";
+				validInput = true;
+				break;
+			case 's':
+				direction = "DOWN";
+				validInput = true;
+				break;
+			case 'd':
+				direction = "RIGHT";
+				validInput = true;
+				break;
+			default:
+				validInput = false;
+				break;
+			}
+		}
+	}
+}
+
+void Logic() {
+
+	if (direction.compare("UP") == 0) {
+		cout << direction << endl;
+		if (movePlayer(pacman, pacman.locationX, pacman.locationY++, GameBoard) == false) {
+			cout << "Need a way to continue in same direction" << endl;
+		}
+	}
+	else if (direction.compare("LEFT") == 0) {
+		cout << direction << endl;
+		GameBoard[pacman.locationX][pacman.locationY] = EMPTY; //current position becomes empty
+		pacman.locationY--;
+		GameBoard[pacman.locationX][pacman.locationY] = PACMAN;
+		//if (movePlayer(pacman, pacman.locationX, pacman.locationY++, GameBoard) == false) {
+		//	cout << "Need a way to continue in same direction" << endl;
+		//}
+	}
+	else if (direction.compare("DOWN") == 0) {
+		cout << direction << endl;
+		GameBoard[pacman.locationX][pacman.locationY] = EMPTY; //current position becomes empty
+		pacman.locationX++;
+		GameBoard[pacman.locationX][pacman.locationY] = PACMAN;
+		//if (movePlayer(pacman, &pacman.locationX, pacman.locationY++, GameBoard) == false) {
+		//	cout << "Need a way to continue in same direction" << endl;
+		//}
+	}
+	else if (direction.compare("RIGHT") == 0) {
+		cout << direction << endl;
+		GameBoard[pacman.locationX][pacman.locationY] = EMPTY; //current position becomes empty
+		pacman.locationY++;
+		GameBoard[pacman.locationX][pacman.locationY] = PACMAN;
+		//if (movePlayer(pacman, pacman.locationX, pacman.locationY++, GameBoard) == false) {
+		//	cout << "Need a way to continue in same direction" << endl;
+		//}
+	}
+	else if (direction.compare("NEW GAME") == 0) {
+		cout << direction << endl;
+		gameOver = true;
+	}
+	else if (direction.compare("QUIT") == 0) {
+		cout << direction << endl;
+		gameOver = true;
+	}
+	else if (direction.compare("PAUSE") == 0) {
+		validInput = false;
+		cout << direction << endl;
+		cout << "Press r to RESUME" << endl;
+		while (validInput == false) {
+			cin >> input;
+			if (cin.fail()) {
+				cin.clear();
+				cin.ignore(256, '\n');
+				validInput = false;
+				cout << "cin failed" << endl;
+			}
+			else {
+				switch (input) {
+				case 'r':
+					validInput = true;
+					cout << "RESUME" << endl;
+					break;
+				default:
+					validInput = false;
+					break;
+				}
+			}
+		}
+	}
+	else if (direction.compare(" ") == 0) {
+		cout << direction << endl;
+		cout << "Something went wrong" << endl;
+	}
+}
+
+int main()
+{
+	Setup();
+	while (gameOver == false)
+	{
+		Draw();
+		Input();
+		Logic();
+	}
+	return 0;
+}
---
+++ Pacman.h
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <stdbool.h>
+#include <fstream>
+#include <string>
+using namespace std;
+
+const int EMPTY = 0;
+const int WALL = 1;
+const int DOT = 2;
+const int SPECIAL_DOT = 3;
+const int CHERRY = 4;
+const int BANANA = 5;
+const int STRAWBERRY = 6;
+const int BLINKY = 7;
+const int PINKY = 8;
+const int INKY = 9;
+const int CLYDE = 10;
+const int PACMAN = 11;
+const int WRAPL = 12;
+const int WRAPR = 13;
+
+struct Player
+{
+	int locationX, locationY, score, lives;
+	Player()
+	{
+		locationX = 12;
+		locationY = 9;
+		score = 0;
+		lives = 3;
+	}
+};
+
+class Ghost
+{
+private:
+	int speed;
+
+};
+
+/*These are arbitrary values, BUT if we keep all
+values greater than 0 wrap-around is much easier
+to implement.
+*/
+
+
+//Do we have access to GameBoard from this function?
+
+bool movePlayer(Player player, int* x, int* y, int GameBoard)
+{
+	int element = GameBoard[x][y];
+
+	if (element == WALL) {
+		return false;
+	}
+	else if ((element == BLINKY) || (element == PINKY) || (element == INKY) || (element == CLYDE)) {
+		player.lives--;
+		//reset level
+		return false;
+	}
+	else if (element == DOT) {
+		player.score++;
+	}
+	else if (element == SPECIAL_DOT) {
+		player.score++;
+		//!!!Need somehow to set ghosts to edible!!!
+	}
+	else if ((element == CHERRY) || (element == BANANA) || (element == STRAWBERRY)) {
+		player.score += 10;
+	}
+
+	else if (element == WRAPL) {
+		GameBoard[&player.locationX][&player.locationX] = EMPTY; //current position becomes empty
+		player.locationX = 10;
+		player.locationY = 18;
+		GameBoard[&player.locationX][&player.locationX] = PACMAN; //desired position becomes player
+		return true;
+	}
+
+	else if (element == WRAPL) {
+		GameBoard[&player.locationX][&player.locationX] = EMPTY; //current position becomes empty
+		player.locationX = 10;
+		player.locationY = 0;
+		GameBoard[&player.locationX][&player.locationX] = PACMAN; //desired position becomes player
+		return true;
+	}
+	/*
+	//Wrap-around
+	else if (x < 0) {	//left to right
+		x = LevelX - 1;
+	}
+	else if (x == LevelX) { //right to left
+		x = 0;
+	}
+	else if (y < 0) { //top to bottom
+		y = LevelY - 1;
+	}
+	else if (y == LevelY) { //bottom to top
+		y = 0;
+	}
+	
+	
+	*/
+	GameBoard[&player.locationX][&player.locationX] = EMPTY; //current position becomes empty
+	player.locationX = *x; player.locationY = *y;
+	GameBoard[&player.locationX][&player.locationX] = PACMAN; //desired position becomes player
+	
+	return true;
+}
+
+
+
