4964aa629a8afcf94a70d84b3ba9a975a2c0cce3
baoloc10 <spiritnumber10@gmail.com>
2016-12-02 21:49:13 -0500

    Add files via upload

--- Pacman.cpp
+++ Pacman.cpp
@@ -6,7 +6,41 @@ bool gameOver;
 bool validInput;
 char input;
 int direction;
+
 Player pacman;
+
+
+struct Ghost
+{
+	int speed, pointValue, locationX, locationY;
+	bool isEdible;
+	double timeIsEdible;
+	Ghost()
+	{
+		isEdible = false;
+		pointValue = 100;
+	}
+ void	*mover(int endx, int endy){
+		int move = solve(locationX, locationY, endx, endy, GameBoard);
+		//left
+		if(move==0){
+			locationX--;
+		}
+		//up
+		else if(move==1){
+			locationY--;
+		}
+		//right
+		else if(move==2){
+			locationX++;
+		}
+		//down
+		else if(move==3){
+			locationY++;
+		}
+	}
+
+};
 
 Ghost Blinky;
 Ghost Pinky;
@@ -14,18 +48,24 @@ Ghost Inky;
 Ghost Clyde;
 
 void Setup() {
-	system("clear");
 	gameOver = false;
-	Blinky.locationX = 10;
-	Blinky.locationY = 9;
-	Pinky.locationX = 9;
-	Pinky.locationY = 9;
-	Inky.locationX = 10;
-	Inky.locationY = 8;
+	Blinky.locationX = 1;
+	Blinky.locationY = 1;
+	Pinky.locationX = 1;
+	Pinky.locationY = 3;
+	Inky.locationX = 20;
+	Inky.locationY = 1;
 	Clyde.locationX = 10;
 	Clyde.locationY = 10;
 	pacman.locationRow = 16;
 	pacman.locationCol = 9;
+
+	GameBoard[Blinky.locationX][Blinky.locationY] = BLINKY;
+	GameBoard[Pinky.locationX][Pinky.locationY] = PINKY;
+	GameBoard[Inky.locationX][Inky.locationY] = INKY;
+	GameBoard[Clyde.locationX][Clyde.locationY] = CLYDE;
+
+
 
 
 	for (int i = 0; i < 22; i++) {
@@ -46,30 +86,70 @@ void Setup() {
 }
 
 void Draw() {
+	system("clear");
 	cout << "SCORE: " << pacman.score << endl;
 	for (int i = 0; i < 22; i++) {
 		for (int j = 0; j < 19; j++) {
+		if((Blinky.locationX == i && Blinky.locationY == j)
+			||(Pinky.locationX == i && Pinky.locationY == j)
+			||(Inky.locationX == i && Inky.locationY == j)
+			||(Clyde.locationX == i && Clyde.locationY == j))	{
+			cout << "GG";
+			}
+		else{
 			if (GameBoard[i][j] == WALL) {
-				cout << "#";
+				cout << "##";
 			}
 			else if (GameBoard[i][j] == DOT) {
-				cout << "*";
+				cout << (char)46 <<(char)46;
 			}
 			else if (GameBoard[i][j] == EMPTY || GameBoard[i][j] == WRAPL || GameBoard[i][j] == WRAPR) {
-				cout << " ";
+				cout << "  ";
 			}
-			else if (GameBoard[i][j] == PINKY || GameBoard[i][j] == INKY || GameBoard[i][j] == BLINKY || GameBoard[i][j] == CLYDE) {
-				cout << "G";
-			}
+			/*else if (GameBoard[i][j] == PINKY || GameBoard[i][j] == INKY || GameBoard[i][j] == BLINKY || GameBoard[i][j] == CLYDE) {
+				cout << "GG";
+			}*/
 			else if (GameBoard[i][j] == PACMAN) {
-				cout << "X";
+				cout << "XX";
 			}
 		}
+	}
+
+
 		cout << endl;
+		for (int j = 0; j < 19; j++) {
+			if((Blinky.locationX == i && Blinky.locationY == j)
+				||(Pinky.locationX == i && Pinky.locationY == j)
+				||(Inky.locationX == i && Inky.locationY == j)
+				||(Clyde.locationX == i && Clyde.locationY == j))	{
+				cout << "GG";
+				}
+			else{
+			if (GameBoard[i][j] == WALL) {
+				cout << "##";
+			}
+			else if (GameBoard[i][j] == DOT) {
+				cout << (char)46 <<(char)46;
+			}
+			else if (GameBoard[i][j] == EMPTY || GameBoard[i][j] == WRAPL || GameBoard[i][j] == WRAPR) {
+				cout << "  ";
+			}
+			/*else if (GameBoard[i][j] == PINKY || GameBoard[i][j] == INKY || GameBoard[i][j] == BLINKY || GameBoard[i][j] == CLYDE) {
+				cout << "GG";
+			}*/
+			else if (GameBoard[i][j] == PACMAN) {
+				cout << "XX";
+			}
+		}
+	}
+		cout<<endl;
 	}
 }
 
+
 void Input() {
+
+
 	validInput = false;
 	direction = HOLD;
 
@@ -123,10 +203,15 @@ void Logic() {
 	int tempX = pacman.locationRow;
 	int tempY = pacman.locationCol;
 
+	bool moved = false;
+
 	switch (direction) {
 	case UP:
 		if (movePlayer(pacman, --tempX, tempY) == true) {
-			pacman.locationRow--;
+			//while(GameBoard[pacman.locationRow][pacman.locationCol] != WALL){
+					pacman.locationRow--;
+
+			//}
 		}
 		else {
 			cout << "Need a way to continue in same direction" << endl;
@@ -200,6 +285,7 @@ int main()
 		Setup();
 		while (gameOver == false)
 		{
+
 			Draw();
 			Input();
 			Logic();
